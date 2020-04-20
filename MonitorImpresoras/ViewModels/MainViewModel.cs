21d790375f0b4e4ee944638ba4e6cecf0e9156d9
Víctor Urdaneta <vurdaneta96@gmail.com>
2020-04-20 18:09:28 -0400

    Ejemplo de binding con comando para cambiar de texto.

--- MonitorImpresoras/ViewModels/MainViewModel.cs
+++ MonitorImpresoras/ViewModels/MainViewModel.cs
@@ -1,13 +1,30 @@
-﻿using MonitorImpresoras.Models;
+﻿using MonitorImpresoras.Helpers;
+using MonitorImpresoras.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace MonitorImpresoras.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         public MainModel MainModel { get; }
+
+        private ICommand _buttonCommand;
+
+        public ICommand ButtonCommand {
+            get {
+                if(_buttonCommand == null)
+                {
+                    _buttonCommand = new RelayCommand((param) =>
+                    {
+                        MainModel.Texto = "Cambio de texto";
+                    });
+                }
+                return _buttonCommand;
+            }
+        }
 
         public MainViewModel()
         {
